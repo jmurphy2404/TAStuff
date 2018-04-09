423d853883e4bb9dfe4b2eee735323ef3e653b6e
John Murphy <murf2073@gmail.com>
2018-04-09 13:56:53 -0400

    final Turtles exercise commit

--- reptile.rb
+++ reptile.rb
@@ -15,5 +15,3 @@ class Reptile
 		end
 	end
 end
-
-
--- turtle.rb
+++ turtle.rb
@@ -18,15 +18,13 @@ class Turtle < Reptile
 		puts "I love the water"
 	end
 
-	def fun_fact
+	def self.fun_fact
 		fun_facts = ["Turtles also have a lower shell called a ‘plastron’.", 
 			"Turtles have existed for around 215 million years",
 			"The largest turtle is the leatherback sea turtle, it can weigh over 900 kg! (2000 lb)",
 			"Many turtle species are endangered.",
 			"Sea turtles have special glands which help remove salt from the water they drink."]
-		temp = Random.new
-		i = temp.rand(0..4)
-		puts fun_facts[i]
+		puts fun_facts[rand(0..4)]
 	end
 end
 
@@ -38,6 +36,6 @@ puts turt.age
 # puts turt.num_limbs
 puts turt.swim
 # puts turt.cold_blooded
-puts turt.fun_fact
+puts Turtle.fun_fact
 puts turt.age!
 puts turt.check_limbs
