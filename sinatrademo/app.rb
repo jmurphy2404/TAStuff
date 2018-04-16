5fb5828b6c26a3fa646d26c0e738dfcb15d74afa
John Murphy <murf2073@gmail.com>
2018-04-16 11:49:36 -0400

    added rubyjokes code to sinatra demo

--- sinatrademo/app.rb
+++ sinatrademo/app.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require 'rubyjokes'
 
 get '/' do
 	erb :index
+end
+
+
+post '/postroute' do
+	joketype = params[:joketype]
+	joke = RubyJoke.new
+	xyz = joke.get_joke({limitTo: [joketype]})
+	erb :index, :locals => {:joke => xyz}
 end
 
