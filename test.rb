5f770da86627c4249a17e3688383cab27bbeaf7c
John Murphy <murf2073@gmail.com>
2018-05-11 17:12:26 -0400

    showing routing examples and their paths

--- test.rb
+++ test.rb
@@ -7,3 +7,10 @@ get '/user/:id' do
 	@specific_user = User.find(params[:id])
 	erb ":/views/show/edit"
 end
+
+
+new_cohort_path cohort#new /cohort/new
+
+cohort_path cohort#show  /cohort/:id
+
+edit_cohort_path cohort#edit /cohort/:id/edit
