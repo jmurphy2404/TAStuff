27666502bf182946adf16fce3fa590883adee4f1
John Murphy <murf2073@gmail.com>
2018-06-26 14:02:00 -0400

    object literal practice

--- jsintro.js
+++ jsintro.js
@@ -1,3 +1,30 @@
 const test = "John Murphy";
 
 let myAge = 28;
+
+let awesomeClass = true;
+
+//
+
+    Create an object with name and eyeColor properties. Change the eye color to brown. Now add a hairColor property and change it to "blonde"
+
+    Create a object literal with your top 3 movies using the keys: topMovieOne, topMovieTwo, topMovieThree.
+
+    Assign some movie values to your keys!
+
+    Access your object literal and print out in the console the following string:
+        My favorite movies are topMovieOne and topMovieTwo, but I really enjoy topMovieThree.
+
+
+
+let obj = {
+	name: "John",
+	eyeColor: "brown",
+	hairColor: "blonde"
+}
+
+let movies = {
+	topMovieOne: "Star Wars",
+	topMovieTwo: "Star Trek",
+	topMovieThree: "Spiderman"
+}
