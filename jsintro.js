b16eea6a2f006bca4b08809cf0a910084ea081c2
John Murphy <murf2073@gmail.com>
2018-06-26 14:38:05 -0400

    basic math operations

--- jsintro.js
+++ jsintro.js
@@ -4,19 +4,6 @@ let myAge = 28;
 
 let awesomeClass = true;
 
-//
-
-    Create an object with name and eyeColor properties. Change the eye color to brown. Now add a hairColor property and change it to "blonde"
-
-    Create a object literal with your top 3 movies using the keys: topMovieOne, topMovieTwo, topMovieThree.
-
-    Assign some movie values to your keys!
-
-    Access your object literal and print out in the console the following string:
-        My favorite movies are topMovieOne and topMovieTwo, but I really enjoy topMovieThree.
-
-
-
 let obj = {
 	name: "John",
 	eyeColor: "brown",
@@ -28,3 +15,15 @@ let movies = {
 	topMovieTwo: "Star Trek",
 	topMovieThree: "Spiderman"
 }
+
+let mathOne = 20;
+
+let mathTwo = 3;
+
+let mathSum = mathOne + mathTwo;
+
+let mathProduct = mathOne * mathTwo;
+
+console.log(mathSum);
+
+console.log(mathProduct);
