9ccacb535ffd0ac14d5a2c89748d8b06a90f7ea3
John Murphy <murf2073@gmail.com>
2018-06-26 11:55:58 -0400

    first day js

---
+++ jsintro.js
@@ -0,0 +1,10 @@
+
+    Create a string variable called test and give it a value of your name
+    Create another variable called myAge and give it a value of your age
+    Add those two together and see what happens! Do you get a string or a number back? How about if we add a number and a boolean?
+    What data type could be used to store both name and age in one variable?
+
+let test = "John Murphy"
+
+let myAge = 28
+
