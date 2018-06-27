0f4d097206571df967a4f8e6368b47a9ec259de7
John Murphy <murf2073@gmail.com>
2018-06-27 13:45:25 -0400

    finished first round exercises

---
+++ function1.js
@@ -0,0 +1,11 @@
+function nycdaWelcome(name) {
+	return `Welcome to NYCDA ${name}! Enjoy your stay!`
+}
+
+function brokenCalcultor(num1, num2) {
+	return num2 * 3
+}
+
+function lineThemUp(int1, int2, int3) {
+	return [int1, int2, int3]
+}
