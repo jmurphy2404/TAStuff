d69580c4c2cd36e6bb97f46529318a2a5f9ffa7b
John Murphy <murf2073@gmail.com>
2018-06-27 15:41:46 -0400

    more js exercises

--- function1.js
+++ function1.js
@@ -9,3 +9,32 @@ function brokenCalcultor(num1, num2) {
 function lineThemUp(int1, int2, int3) {
 	return [int1, int2, int3]
 }
+
+let royalizer = (gender, name) => {
+  if(gender == "male"){
+    return `His Majesty ${name}`
+  }else{
+    return `Her Majesty ${name}`
+  }
+}
+
+royalizer("male", "john");
+royalizer("female", "ann");
+
+let magicDoor = (num) => {
+  if(num == 1){
+    message = "you won a car"
+  }else if (num ==2) {
+    message = "you won a tshirt"
+  }else if (num == 3) {
+    message = "sorry you got teleported to antarctica"
+  }else {
+    return "Not a valid input"
+  }
+  alert(message);
+}
+
+magicDoor(1);
+magicDoor(2);
+magicDoor(3);
+magicDoor(4);
