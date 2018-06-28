fef82addfff261ff80e3f7bb55987e24891101aa
John Murphy <murf2073@gmail.com>
2018-06-28 15:43:57 -0400

    updated movie flight to fix loop ending condition

--- flightMovies.js
+++ flightMovies.js
@@ -1,7 +1,7 @@
 arr = [50, 90, 102, 180, 165, 70]
 
 let movieCalc = (movies, airtime) => {
-  for(i=0; i < (movies.length); i++){
+  for(i=0; i < (movies.length - 1); i++){
     for(j=1; j < (movies.length); j++)
     if(movies[i] + movies[j] == airtime){
       return true;
