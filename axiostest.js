abeeb617b488db9b3724aad3bc1a89dfb7c3297b
John Murphy <murf2073@gmail.com>
2018-07-12 14:41:13 -0400

    response logged from API for axiosdemo

---
+++ axiostest.js
@@ -0,0 +1,11 @@
+// Given the following URL : https://api.myjson.com/bins/bygvt containing a JSON object about your favorite GOT character.
+// Create a AJAX call for that url to grab the JSON data and log the result in your console.
+// Loop through the aliases array and console log out the results
+// Return the value of 'actor' in the 'playedBy' object.
+// Append the name of the actor to the dom to a p element with the id of "my-actor"
+
+// const axios = require('axios');
+
+axios.get('https://api.myjson.com/bins/bygvt').then((response) => {
+	console.log(response);
+})
