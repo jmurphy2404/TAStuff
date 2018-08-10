f9f49e2eefef65472446d72de6b5e71c0b55648b
John Murphy <murf2073@gmail.com>
2018-08-10 13:59:09 -0400

    showing how wildcard routes interfere with similar static routes

--- sinatramvc/app.rb
+++ sinatramvc/app.rb
@@ -9,3 +9,12 @@ get '/' do
     @cats = Cat.all()
     erb :index
 end
+
+
+get '/cat/:id' do
+	# some code
+end
+
+get '/cat/new' do
+	#some code that won't run unless this is moved above the '/cat/:id' route
+end
